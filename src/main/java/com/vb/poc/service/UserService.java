e2f34df1fdd7963acf32f4bcb4d63e9c66c5492d
imran ansari <stasim101@gmail.com>
2021-05-02 21:34:03 +0530

    Adding UserService

---
+++ src/main/java/com/vb/poc/service/UserService.java
@@ -0,0 +1,30 @@
+package com.vb.poc.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.vb.poc.model.User;
+
+@Service
+public class UserService {
+
+	private User createUser(String firstname, String lastname, int age) {
+		return null;
+	}
+
+	private User retrieveUser(String firstname, String lastname) {
+		return null;
+	}
+
+	private List<User> retrieveUserList() {
+		return null;
+	}
+
+	private User updateUser(long id, User user) {
+		return null;
+	}
+
+	private User deleteUser(long id) {
+		return null;
+	}
+
+}
