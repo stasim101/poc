bb89b941cf765d44da79a192a0032330e0894d7c
imran ansari <stasim101@gmail.com>
2021-05-02 21:19:10 +0530

    Adding User model class

---
+++ src/main/java/com/vb/poc/model/User.java
@@ -0,0 +1,45 @@
+package com.vb.poc.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+
+	private long id;
+	private String firstname;
+	private String lastname;
+	private int age;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
